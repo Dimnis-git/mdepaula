0c0db4847712e158975c30e240acc7a81eb5d58e
Renan Oliveira <rdpereira2010@hotmail.com>
2025-08-24 19:39:16 -0300

    📲 improve responsive on header

--- src/lib/components/layout/header.svelte
+++ src/lib/components/layout/header.svelte
@@ -3,11 +3,11 @@
 	import Icon from '../icon.svelte';
 </script>
 
-<header class="flex justify-between items-center p-2">
+<header class="flex justify-between items-center m-4">
 	<div>
-		<img src={Logo} alt="Logo" class="w-28 drop-shadow-sm drop-shadow-yellow-700/60"/>
+		<img src={Logo} alt="Logo" class="w-28 lg:w-48 drop-shadow-sm drop-shadow-yellow-700/60"/>
 	</div>
-	<div class="flex item gap-2">
+	<div class="flex item gap-4">
 		{@render HeaderLinks(
 			'https://api.whatsapp.com/send?phone=11995943124',
 			'ic:baseline-whatsapp',
@@ -18,7 +18,7 @@
 </header>
 
 {#snippet HeaderLinks(href: string, icon: string, title: string)}
-	<a {href} {title} class="p-1.5 rounded-xl text-white drop-shadow-sm {title === 'Instagram' ? 'bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow-pink-400' : 'bg-green-500 drop-shadow-green-400'}">
-		<Icon {icon} class="text-lg"/>
+	<a {href} {title} class="p-1.5 lg:p-3 rounded-full text-white drop-shadow-sm {title === 'Instagram' ? 'bg-gradient-to-r from-purple-500 to-pink-500 drop-shadow-pink-400' : 'bg-green-500 drop-shadow-green-400'}">
+		<Icon {icon} class="text-lg lg:text-4xl"/>
 	</a>
 {/snippet}
