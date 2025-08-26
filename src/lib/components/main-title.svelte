e01c867879a2999c143e87397acdf54cb306a241
Renan Oliveira <rdpereira2010@hotmail.com>
2025-08-25 22:45:38 -0300

    🔍 change main title for try to improve SEO

--- src/lib/components/main-title.svelte
+++ src/lib/components/main-title.svelte
@@ -2,8 +2,11 @@
 	import Icon from "./icon.svelte";
 </script>
 
-<section class="flex items-center justify-center lg:gap-4 mb-4">
-	<Icon icon="fluent-emoji:sparkles" class="text-5xl"/>
-	<h1 class="font-cal font-medium lg:text-2xl text-center px-2">Transformando ideias em móveis que fazem parte da sua história.</h1>
-	<Icon icon="fluent-emoji:couch-and-lamp" class="text-5xl"/>
+<section class="flex items-center justify-center lg:gap-8 mb-4 gap-2">
+	<Icon icon="fluent-emoji:sparkles" class="text-6xl"/>
+	<div>
+		<h1 class="text-center font-figtree font-semibold max-lg:text-[10px]">Marcenaria de Móveis Planejados</h1>
+		<h2 class="!mt-0 max-lg:!text-xs">Transformando ideias em móveis que fazem parte da sua história.</h2>
+	</div>
+	<Icon icon="fluent-emoji:couch-and-lamp" class="text-6xl"/>
 </section>
