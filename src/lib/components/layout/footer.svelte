e9a13054f9faf61fa162179686b1c0ace72d43d7
Renan Oliveira <rdpereira2010@hotmail.com>
2025-08-25 22:38:00 -0300

    💬 add location

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -1,12 +1,13 @@
-<footer class="p-1 text-center font-figtree text-xs mt-6">
+<footer class="p-1 text-center font-figtree mt-6">
 	<button
 		type="button"
-		class="mb-2 h-8 border-black pt-3 text-sm font-medium hover:border-b cursor-pointer"
+		class="mb-4 h-8 border-black pt-3 font-medium hover:border-b cursor-pointer"
 		onclick={() =>
 			window.scrollTo({
                 top: 0,
                 behavior: 'smooth'
             })}>Voltar ao topo da página</button
 	>
-	<p>Marcenaria de Paula {new Date().getFullYear()} &copy; Todos os direitos reservados</p>
+	<p class="text-sm">Marcenaria de Paula {new Date().getFullYear()} &copy; Todos os direitos reservados</p>
+	<p class="text-xs my-1 text-current/70">R. Dom Sebastião - Brasilândia, São Paulo - SP, 02847-087</p>
 </footer>
